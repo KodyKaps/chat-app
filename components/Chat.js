76423a68daecfe514622835255b3f037fe285a07
Kody Kapetanis <kodykapetanis@yahoo.com>
2025-02-22 15:03:13 -0500

    Components

--- components/Chat.js
+++ components/Chat.js
@@ -1,10 +1,48 @@
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Platform, KeyboardAvoidingView } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
 export default function ChatScreen({route}){
     const {text, color} = route.params
+    const [messages, setMessages] = useState([])
+    const onSend = (newMessages) => {
+        setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+    }
+    useEffect(() => {
+        setMessages([
+            {
+                _id: 1,
+                text: 'Hi nice to meet you',
+                createdAt: new Date(),
+                user: {
+                    _id: 2,
+                    name: "Computer",
+                    avatar: "https://placeimg.com/140/140/any"
+                }
+            },
+            {
+                _id: 2,
+                text: 'Here is my ip address',
+                createdAt: new Date(),
+                user: {
+                    _id: 2,
+                    name: "Computer",
+                    avatar: "https://placeimg.com/140/140/any"
+                }
+            }
+        ])
+    },[])
     return (
         <View style={[styles.container, {backgroundColor: color}]}>
             
-            <Text>Chat screen{text} {color}</Text>
+            <GiftedChat
+                messages={messages}
+                onSend={messages => onSend(messages)}
+                user={{
+                    _id: 1
+                }}
+            />
+            {Platform.OS === 'android' ? <KeyboardAvoidingView behavior='height'/> : null}
+            {Platform.OS === 'ios' ? <KeyboardAvoidingView behavior='padding'/> : null}
         </View>
     )
 }
@@ -13,7 +51,6 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+      
     },
   });
