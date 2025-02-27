6de39b5ffabf0ba51b24a32c4d3b5ed83b5ca9be
Kody Kapetanis <kodykapetanis@yahoo.com>
2025-02-26 21:07:30 -0500

    5.5 Create communication features

--- components/Chat.js
+++ components/Chat.js
@@ -1,13 +1,15 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Platform, KeyboardAvoidingView } from 'react-native';
+import { StyleSheet, Button, Text, Image, View, Platform, KeyboardAvoidingView } from 'react-native';
 import { GiftedChat, InputToolbar } from 'react-native-gifted-chat';
 import {collection, query, addDoc, onSnapshot, orderBy} from "firebase/firestore"
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+import MapView from 'react-native-maps';
+import CustomActions from './CustomActions';
 export default function ChatScreen({route, db, isConnected}){
     const {color, userID, name} = route.params
     const [messages, setMessages] = useState([])
-
+    const [image, setImage] = useState(null)
     const onSend = (newMessages) => {
         //store directly in fb
         addDoc(collection(db, "messages"), newMessages[0])
@@ -64,8 +66,46 @@ export default function ChatScreen({route, db, isConnected}){
         if (isConnected) return <InputToolbar {...props} />;
         else return null;
     }
+    
+    
+    const renderCustomActions = (props) => {
+        return (
+            <CustomActions {...props}/>
+        )
+    }
+    const renderCustomView = (props) => {
+        const { currentMessage} = props;
+        if (currentMessage.location) {
+          return (
+              <MapView
+                style={{width: 150,
+                  height: 100,
+                  borderRadius: 13,
+                  margin: 3}}
+                region={{
+                  latitude: currentMessage.location.latitude,
+                  longitude: currentMessage.location.longitude,
+                  latitudeDelta: 0.0922,
+                  longitudeDelta: 0.0421,
+                }}
+              />
+          );
+        }
+        return null;
+    }
     return (
         <View style={[styles.container, {backgroundColor: color}]}>
+            {/* <Button
+                title= "Pick an image from the library"
+                onPress={pickImage}
+            />
+            <Button
+                title= "Take a photo"
+                onPress={takePhoto}
+            />
+            {image &&
+            <Image source={{ uri: image.uri }} style={{ width: 200, height: 200 }} />
+            } */}
             
             <GiftedChat
                 messages={messages}
@@ -75,6 +115,8 @@ export default function ChatScreen({route, db, isConnected}){
                     name: name
                 }}
                 renderInputToolbar={renderInputToolbar}
+                renderActions={renderCustomActions}
+                renderCustomView={renderCustomView}
             />
             {Platform.OS === 'android' ? <KeyboardAvoidingView behavior='height'/> : null}
             {Platform.OS === 'ios' ? <KeyboardAvoidingView behavior='padding'/> : null}
